943047599850f2cfc47ef36a7533e8ee2bd21f39
slvzk7 <enzo.guimaraes2@aluno.educa.go.gov.br>
2026-02-14 11:05:22 -0300

    components.js

---
+++ components.js
@@ -0,0 +1,14 @@
+document.getElementById("header").innerHTML = `
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="#">Lojinha do SLV</a>
+  </div>
+</nav>
+`;
+
+document.getElementById("footer").innerHTML = `
+<footer class="text-center">
+  <p>© 2026 Lojinha do SLV</p>
+  <p>Projeto para portfólio - Desenvolvido por Silva</p>
+</footer>
+`;
